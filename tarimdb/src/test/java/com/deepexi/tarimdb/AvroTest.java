34ee4ba7d8aa4e623bbb1307e70db43d9c3fd044
Deepliu <me@liudonghua.net>
2023-03-09 19:50:13 +0800

    1.tarimdb protocol define; 2.AVRO test code;

--- tarimdb/src/test/java/com/deepexi/tarimdb/AvroTest.java
+++ tarimdb/src/test/java/com/deepexi/tarimdb/AvroTest.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.io.ByteArrayOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.EOFException;
+import java.lang.ArrayIndexOutOfBoundsException;
 
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
@@ -39,6 +40,16 @@ class AvroTest {
                                      + "    {\"name\": \"f2\", \"type\": \"boolean\"}"
                                      + "]}";
 
+    private final String schema2_json = "{\"namespace\": \"org.apache.arrow.avro\","
+                                      + "\"type\": \"record\","
+                                      + "\"name\": \"TarimRecord\","
+                                      + "\"fields\": ["
+                                      + "    {\"name\": \"f0\", \"type\": \"string\"},"
+                                      + "    {\"name\": \"f1\", \"type\": \"int\"},"
+                                      + "    {\"name\": \"f2\", \"type\": \"boolean\"},"
+                                      + "    {\"name\": \"_fv\", \"type\": [\"int\", \"null\"], \"default\": 0}"
+                                      + "]}";
+
     @Test
     void contextLoads() {
     }
@@ -57,6 +68,134 @@ class AvroTest {
         Schema schema = new Schema.Parser().parse(schema_json);
         ArrayList<GenericRecord> data = new ArrayList<>();
         int writeSize = 0;
+        for (int i = 8; i < 15; i++) 
+        {
+            GenericRecord record = new GenericData.Record(schema);
+            record.put(0, "test" + i);
+            record.put(1, i);
+            record.put(2, i % 2 == 0);
+            data.add(record);
+            writeSize++;
+        }
+
+        logger.info("schema in avro: " + schema.toString(true));
+        logger.info("data size: " + data.size());
+        Assertions.assertEquals(writeSize, data.size());
+
+        ByteArrayOutputStream bytesOS = new ByteArrayOutputStream();
+        BinaryEncoder encoder = new EncoderFactory().directBinaryEncoder(bytesOS, null); // or binaryEncoder() to create BufferedBinaryEncoder
+        DatumWriter writer = new GenericDatumWriter(schema);
+
+        int lastSize = 0;
+        //ArrayList<byte[]> dataList = new ArrayList<>();
+        for(GenericRecord record : data)
+        {
+            writer.write(record, encoder);
+            logger.info("bytes buffer size: " + bytesOS.size() + ", add size: " + (bytesOS.size() - lastSize));
+            lastSize = bytesOS.size();
+            //byte[] bb = bytesOS.toByteArray();
+            //dataList.add(bb);
+            //logger.info("every bytes buffer size: " + );
+        }
+
+        byte[] recordBuf = bytesOS.toByteArray();
+        logger.info("data size after encode: " + recordBuf.length);
+
+        ByteArrayInputStream bytesIS = new ByteArrayInputStream(recordBuf);
+        BinaryDecoder decoder = new DecoderFactory().directBinaryDecoder(bytesIS, null);
+        GenericDatumReader<GenericRecord> reader = new GenericDatumReader<>();
+        reader.setSchema(schema);
+
+        int readSize = 0;
+        while(true)
+        {
+            try {
+                GenericRecord datum = reader.read(null, decoder);
+                if(datum == null) break;
+                readSize++;
+                logger.info(readSize + ": " + datum.get("f0") + ", " + datum.get("f1") + ", " + datum.get("f2")
+                                     + ". Datum: " + datum.toString());
+            }catch(EOFException e){
+                logger.info("read datum complete.");
+                break;
+            }
+        }
+
+        Assertions.assertEquals(writeSize, readSize);
+    }
+
+    @Test
+    public void testRecords2() throws Exception 
+    {
+        Schema schema = new Schema.Parser().parse(schema_json);
+        ArrayList<GenericRecord> data = new ArrayList<>();
+        int writeSize = 0;
+        for (int i = 8; i < 15; i++) 
+        {
+            GenericRecord record = new GenericData.Record(schema);
+            record.put(0, "test" + i);
+            record.put(1, i);
+            record.put(2, i % 2 == 0);
+            data.add(record);
+            writeSize++;
+        }
+
+        logger.info("schema in avro: " + schema.toString(true));
+        logger.info("data size: " + data.size());
+        Assertions.assertEquals(writeSize, data.size());
+
+        ByteArrayOutputStream bytesOS = new ByteArrayOutputStream();
+        BinaryEncoder encoder = new EncoderFactory().directBinaryEncoder(bytesOS, null); // or binaryEncoder() to create BufferedBinaryEncoder
+        DatumWriter writer = new GenericDatumWriter(schema);
+
+        int lastSize = 0;
+        ArrayList<byte[]> dataList = new ArrayList<>();
+        for(GenericRecord record : data)
+        {
+            writer.write(record, encoder);
+            logger.info("bytes buffer size: " + bytesOS.size() + ", add size: " + (bytesOS.size() - lastSize));
+            lastSize = bytesOS.size();
+            byte[] bb = bytesOS.toByteArray();
+            dataList.add(bb);
+            logger.info("every bytes buffer size: " + bb.length);
+            bytesOS.reset();
+        }
+
+        //byte[] recordBuf = bytesOS.toByteArray();
+        //logger.info("data size after encode: " + recordBuf.length);
+
+        //ByteArrayInputStream bytesIS = new ByteArrayInputStream(recordBuf);
+        GenericDatumReader<GenericRecord> reader = new GenericDatumReader<>();
+        reader.setSchema(schema);
+
+        int readSize = 0;
+        BinaryDecoder decoder = new DecoderFactory().binaryDecoder(dataList.get(readSize), 0, dataList.get(readSize).length, null);
+        while(true)
+        {
+            try {
+                //decoder = new DecoderFactory().directBinaryDecoder(bytesIS, decoder);
+                if(readSize > 0) decoder = new DecoderFactory().binaryDecoder(dataList.get(readSize), 0, dataList.get(readSize).length, decoder);
+                GenericRecord datum = reader.read(null, decoder);
+                if(datum == null) break;
+                readSize++;
+                logger.info(readSize + ": " + datum.get("f0") + ", " + datum.get("f1") + ", " + datum.get("f2")
+                                     + ". Datum: " + datum.toString());
+                if(readSize >= dataList.size()) break;
+            }catch(EOFException e){
+                logger.info("read datum complete.");
+                break;
+            }
+        }
+
+        Assertions.assertEquals(writeSize, readSize);
+    }
+
+    @Test
+    public void testSchemaDiff() throws Exception 
+    {
+        Schema schema = new Schema.Parser().parse(schema_json);
+        ArrayList<GenericRecord> data = new ArrayList<>();
+        int writeSize = 0;
         for (int i = 0; i < 5; i++) 
         {
             GenericRecord record = new GenericData.Record(schema);
@@ -68,7 +207,7 @@ class AvroTest {
         }
 
         logger.info("schema in avro: " + schema.toString(true));
-        logger.info("testSchema() data size: " + data.size());
+        logger.info("data size: " + data.size());
         Assertions.assertEquals(writeSize, data.size());
 
         ByteArrayOutputStream bytesOS = new ByteArrayOutputStream();
@@ -78,30 +217,36 @@ class AvroTest {
         for(GenericRecord record : data)
         {
             writer.write(record, encoder);
+            logger.info("bytes buffer size: " + bytesOS.size());
         }
 
         byte[] recordBuf = bytesOS.toByteArray();
-        logger.info("testSchema() data size after encode: " + recordBuf.length);
+        logger.info("data size after encode: " + recordBuf.length);
 
         ByteArrayInputStream bytesIS = new ByteArrayInputStream(recordBuf);
         BinaryDecoder decoder = new DecoderFactory().directBinaryDecoder(bytesIS, null);
-        GenericDatumReader<Object> reader = new GenericDatumReader<>();
-        reader.setSchema(schema);
+        GenericDatumReader<GenericRecord> reader = new GenericDatumReader<>();
+        Schema schema2 = new Schema.Parser().parse(schema2_json);
+        reader.setSchema(schema2);
 
         int readSize = 0;
         while(true)
         {
             try {
-                Object datum = reader.read(null, decoder);
+                GenericRecord datum = reader.read(null, decoder);
                 if(datum == null) break;
                 readSize++;
-                logger.info("datum: " + datum.toString());
+                logger.info(readSize + ": " + datum.get("f0") + ", " + datum.get("f1") + ", " + datum.get("f2")
+                                     + ". Datum: " + datum.toString());
             }catch(EOFException e){
                 logger.info("read datum complete.");
                 break;
+            }catch(ArrayIndexOutOfBoundsException e){
+                logger.info("schema parse error.");
+                Assertions.assertFalse(false);
+                break;
             }
+            Assertions.assertFalse(true);
         }
-
-        Assertions.assertEquals(writeSize, readSize);
     }
 }
